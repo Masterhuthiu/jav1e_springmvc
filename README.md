1bc32c3342c0ecf98419b5dbac6a031da4a3e431
masterhuthiu@gmail <masterhuthiu@gmail.com>
2024-10-18 11:53:56 +0700

    1commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # jav1e_springmvc
+tao spring project dep:thy, web
+tao file index.html trong thu muc static
+run spring boot
---
+++ jav1e_springmvc/src/main/java/com/example/jav1e_springmvc/Jav1eSpringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.jav1e_springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jav1eSpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jav1eSpringmvcApplication.class, args);
+	}
+
+}
